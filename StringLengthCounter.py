4461aaf4014c0a7234997310ebe20975ecfb3152
Saiful Islam <72754002+saiful2516@users.noreply.github.com>
2024-02-29 03:34:56 +0100

    converting string into list of user input
    Python basic user input string and convert it into list.

---
+++ StringLengthCounter.py
@@ -0,0 +1,13 @@
+text = input("Enter a text to be counted the length: ")
+lenght = len(text)
+print("The length of the title: ", lenght)
+
+catch_input = input("enter your messege:")
+print(catch_input)
+
+my_variable1 = "name:saiful"
+my_variable2 = "age:32"
+my_variable3 = "Occupation: student job"
+myvariable4 = "Hoby: travelling"
+my_variable = [my_variable1, my_variable2, my_variable3, myvariable4]
+print(my_variable)
