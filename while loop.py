13390638028feec1b00119abc145643ea05eb70c
Saiful Islam <72754002+saiful2516@users.noreply.github.com>
2024-03-02 14:04:36 +0100

    While loop and list and append function.
    In this code, we run the while loop and basic concept of it. Secondly, we have store string file using the append function in the list. We did some experiment for commenting and uncommenting with use of ctr+o+/.   For selecting line movement together, we have used short key Ctrl + shift+ upper/lower arrow.

---
+++ while loop.py
@@ -0,0 +1,22 @@
+
+user_prompt = "Enter a todo:"
+#boolean data type true or false(string or list).
+# while 2 > 1:
+#     todo = input(user_prompt)
+#     print(todo)
+#     print("Next....")
+
+# while True:
+#     todo = input(user_prompt)
+#     print(todo)
+#     print("Next....")
+user_prompt = "Enter a todo:"
+todos = []
+
+while True:
+    todo = input(user_prompt)
+    todos.append(todo)
+    print(todos)
+
+
+
