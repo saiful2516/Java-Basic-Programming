7524d70a706f242a82319cf3b313513c9bc23dcc
Saiful Islam <72754002+saiful2516@users.noreply.github.com>
2024-03-07 00:10:24 +0100

    Currency converstion and finding name by rank

---
+++ Dollar_euro_conversion.py
@@ -0,0 +1,25 @@
+rate = 2
+
+dollar_amount = int(input("Enter your amount: "))
+Euros = dollar_amount * rate
+print(Euros)
+
+ranking = ['John', 'Sen', 'Lisa']
+
+
+number = int(input("Enter rank number: "))
+number = number - 1
+print(ranking[number])
+
+for number in ranking:
+        # TODO: write code...
+        number = int(input("Enter rank number: "))
+        number = number - 1
+        print(ranking[number])
+
+mood = "name"
+strength = float(32.25)
+rank = int(10)
+print(mood)
+print(strength)
+print(rank)
